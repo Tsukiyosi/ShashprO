c70925b2c9192f10d8d8f12ad5cad9dc809afa69
Tsuk1 <tsukiyoshiae@gmail.com>
2022-12-27 00:06:54 +0700

    ShashprO

---
+++ ShoprO/MainMenu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoprO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void SinglPl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MultiPl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Seting_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
